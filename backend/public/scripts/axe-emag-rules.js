95f6397927b99a1e883ab855d624a31323ccaebe
thiagoexe <thiagobriitto@outlook.com>
2025-05-22 04:14:57 -0300

    feat: adicionar regras 1.1.4, 1.1.3 do ASES

--- backend/public/scripts/axe-emag-rules.js
+++ backend/public/scripts/axe-emag-rules.js
@@ -1,5 +1,6 @@
 (() => {
   const emagChecks = [
+    // Alt nas imagens
     {
       id: "imagem-alt-emag",
       evaluate: function (node) {
@@ -8,6 +9,7 @@
         );
       },
     },
+    // Ancora para bloco
     {
       id: "ancora-para-bloco",
       evaluate: function (node) {
@@ -35,6 +37,7 @@
         return true;
       },
     },
+    // Accesskeys únicas
     {
       id: "accesskey-unico",
       evaluate: function () {
@@ -47,6 +50,7 @@
         return duplicates.length === 0;
       },
     },
+    // Primeiro link da pagina deve pular pro conteudo
     {
       id: "primeiro-link-para-conteudo",
       evaluate: function () {
@@ -77,6 +81,21 @@
 
         const idsAceitaveis = ["conteudo", "main", "principal", "content"];
         return idsAceitaveis.includes(targetId.toLowerCase());
+      },
+    },
+    // verificando css-inline
+    {
+      id: "css-inline-check",
+      evaluate: function (node) {
+        // Se existe atributo style, retorna true
+        return node.hasAttribute("style");
+      },
+    },
+    // Validar css interno
+    {
+      id: "css-internal-check",
+      evaluate: function (node) {
+        return node.tagName.toLowerCase() === "style";
       },
     },
   ];
@@ -112,6 +131,38 @@
           "Deve haver âncoras acessíveis no início da página que permitam pular para blocos de conteúdo como menu, conteúdo principal e busca.",
         help: "EMAG 3.1 R1.5 - Falta de âncoras acessíveis.",
         helpUrl: "https://emag.governoeletronico.gov.br/#r1.5",
+      },
+    },
+    {
+      id: "css-inline",
+      selector: "[style]",
+      any: ["css-inline-check"],
+      all: [],
+      none: [],
+      enabled: true,
+      tags: ["emag", "padrões", "html"],
+      impact: "minor",
+      metadata: {
+        description:
+          "Evitar o uso de CSS inline. Respeite os padrões Web HTML. Utilize folhas de estilo externas.",
+        help: "EMAG 3.1 R1.1.3 - Presença de CSS(s) in-line.",
+        helpUrl: "https://emag.governoeletronico.gov.br/#r1.1",
+      },
+    },
+    {
+      id: "css-internal",
+      selector: "style",
+      any: ["css-internal-check"],
+      all: [],
+      none: [],
+      enabled: true,
+      tags: ["emag", "padrões", "html"],
+      impact: "minor",
+      metadata: {
+        description:
+          "Evitar o uso de CSS interno. Respeite os padrões Web HTML. Utilize folhas de estilo externas.",
+        help: "EMAG 3.1 R1.1.4 - Presença de CSS(s) interno.",
+        helpUrl: "https://emag.governoeletronico.gov.br/#r1.1",
       },
     },
   ];
